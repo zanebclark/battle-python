269909c1b00e5c57c712cd923c5753e6cf89d894
zanebclark <zanebclark@gmail.com>
2023-12-31 22:20:52 -0800

    fix: use the right function for environment variables

--- scripts/get_cloudformation_outputs.py
+++ scripts/get_cloudformation_outputs.py
@@ -22,7 +22,7 @@ def main(stack_name: str):
     outputs = response["Stacks"][0]["Outputs"]
 
     env_vars = [get_formatted_output(output) for output in outputs]
-    with open(os.getenv["GITHUB_OUTPUT"], "a") as fh:
+    with open(os.environ["GITHUB_OUTPUT"], "a") as fh:
         for env_var in env_vars:
             print(env_var)
             print(f"{env_var}", file=fh)
