c3492e9bc82f514f0ba53647c2a45cbdd10120f6
zanebclark <zanebclark@gmail.com>
2024-03-06 21:39:33 -0800

    feat: implement logging wrapper

--- src/battle_python/utils.py
+++ src/battle_python/utils.py
@@ -1,5 +1,14 @@
+import functools
+import json
+from typing import Literal
+
 import numpy as np
 import numpy.typing as npt
+import structlog
+from logger_cloudwatch_structlog import setup_and_get_logger, AWSCloudWatchLogs
+
+log_levels = {"critical", "error", "warning", "info", "debug"}
+LogLevel = Literal["critical", "error", "warning", "info", "debug"]
 
 
 def get_aligned_masked_array(
@@ -42,3 +51,58 @@ def get_aligned_masked_array(
             for row in masked_array
         ]
     )
+
+
+def setup_and_get_logger_with_processors(log_level: str = "DEBUG"):
+    processors = (
+        structlog.stdlib.filter_by_level,
+        structlog.stdlib.add_log_level,
+        structlog.stdlib.PositionalArgumentsFormatter(),
+        structlog.processors.TimeStamper(fmt="iso"),
+        structlog.processors.StackInfoRenderer(),
+        structlog.processors.dict_tracebacks,
+        structlog.processors.UnicodeDecoder(),
+        structlog.threadlocal.merge_threadlocal,
+        AWSCloudWatchLogs(
+            callouts=["status_code", "event"], serializer=json.dumps, sort_keys=False
+        ),
+    )
+    return setup_and_get_logger(processors=processors, level=log_level)
+
+
+def log_fn(_func=None, *, logger: structlog.BoundLogger, log_level: LogLevel = "debug"):
+    def generic_log_fn(func):
+        @functools.wraps(func)
+        def wrapper(*args, **kwargs):
+            # noinspection PyProtectedMember
+            logger._proxy_to_logger(
+                log_level,
+                f"{func.__name__} called",
+                args=", ".join([repr(arg) for arg in args]),
+                **kwargs,
+            )
+
+            try:
+                result = func(*args, **kwargs)
+                # noinspection PyProtectedMember
+                logger._proxy_to_logger(
+                    log_level, f"{func.__name__} returned", result=result
+                )
+                return result
+            except Exception as e:
+                logger.exception(
+                    f"Exception raised in {func.__name__}. exception: {str(e)}"
+                )
+                raise e
+
+        return wrapper
+
+    if log_level not in log_levels:
+        raise ValueError(
+            f"unhandled log_level: {log_level}. Supported values: {', '.join(log_levels)}"
+        )
+
+    if _func is None:
+        return generic_log_fn
+
+    return generic_log_fn(_func)
---
+++ tests/unit/test_utils.py
@@ -0,0 +1,146 @@
+import logging
+
+import pytest
+import structlog
+from structlog.testing import capture_logs
+
+from battle_python.utils import setup_and_get_logger_with_processors, log_fn
+
+
+def get_logger(log_level):
+    structlog.reset_defaults()
+    structlog.configure(
+        processors=[structlog.processors.JSONRenderer()],
+        wrapper_class=structlog.make_filtering_bound_logger(log_level),
+        cache_logger_on_first_use=False,
+    )
+    return structlog.get_logger()
+
+
+@pytest.fixture()
+def debug_logger():
+    return get_logger(logging.DEBUG)
+
+
+@pytest.fixture()
+def info_logger():
+    return get_logger(logging.INFO)
+
+
+def test_log_fn_no_log_level(debug_logger):
+    arg = "something"
+    kwarg = {"keyword": "argument"}
+    result = "some_result"
+
+    @log_fn(logger=debug_logger)
+    def foo(*args, **kwargs):
+        return result
+
+    # noinspection PyArgumentList
+    with capture_logs() as cap_logs:
+        foo(arg, **kwarg)
+
+    assert {
+        "args": f"'{arg}'",
+        "event": "foo called",
+        "log_level": "debug",
+        **kwarg,
+    } in cap_logs
+
+    assert {
+        "event": "foo returned",
+        "log_level": "debug",
+        "result": result,
+    } in cap_logs
+
+
+def test_log_fn_supplied_log_level(info_logger):
+    arg = "something"
+    kwarg = {"keyword": "argument"}
+    result = "some_result"
+
+    @log_fn(logger=info_logger, log_level="info")
+    def foo(*args, **kwargs):
+        return result
+
+    # noinspection PyArgumentList
+    with capture_logs() as cap_logs:
+        foo(arg, **kwarg)
+
+    assert {
+        "args": f"'{arg}'",
+        "event": "foo called",
+        "log_level": "info",
+        **kwarg,
+    } in cap_logs
+
+    assert {
+        "event": "foo returned",
+        "log_level": "info",
+        "result": result,
+    } in cap_logs
+
+
+# TODO: See skip note. Fix this
+@pytest.mark.skip(
+    "I can't figure out how to properly configure log level filtering when testing"
+)
+def test_log_fn_not_logged(info_logger):
+    arg = "something"
+    kwarg = {"keyword": "argument"}
+    result = "some_result"
+
+    @log_fn(logger=info_logger, log_level="debug")
+    def foo(*args, **kwargs):
+        return result
+
+    # noinspection PyArgumentList
+    with capture_logs() as cap_logs:
+        foo(arg, **kwarg)
+
+    assert len(cap_logs) == 0
+
+
+def test_log_fn_invalid_log_level(debug_logger):
+    arg = "something"
+    kwarg = {"keyword": "argument"}
+    result = "some_result"
+
+    with pytest.raises(ValueError) as e:
+        # noinspection PyArgumentList
+        with capture_logs() as cap_logs:
+
+            @log_fn(logger=debug_logger, log_level="DEBUG2")
+            def foo(*args, **kwargs):
+                return result
+
+    assert "unhandled log_level" in str(e.value)
+
+
+def test_log_fn_function_exception(debug_logger):
+    arg = "something"
+    kwarg = {"keyword": "argument"}
+    result = "some_result"
+
+    @log_fn(logger=debug_logger)
+    def foo(*args, **kwargs):
+        raise Exception("some exception")
+
+    with pytest.raises(Exception) as e:
+        # noinspection PyArgumentList
+        with capture_logs() as cap_logs:
+            foo(arg, **kwarg)
+
+    assert "some exception" in str(e.value)
+    assert {
+        "args": f"'{arg}'",
+        "event": "foo called",
+        "log_level": "debug",
+        **kwarg,
+    } in cap_logs
+
+    assert {
+        "event": "Exception raised in foo. exception: some exception",
+        "exc_info": True,
+        "log_level": "error",
+    } in cap_logs
